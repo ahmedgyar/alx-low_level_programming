63ceebc94afa2dc4f273d2272db914aaad77aa79
ahmedgyar <you@example.com>
2023-10-10 15:18:07 -0700

    C is cool

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+	printf("%c", ch);
+	}
+	printf("\n");
+	}
+
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int)
+{
+
+	if (n > 0)
+	{
+	n = (-1) * n
+		return (n);
+	}
+
+
+}
+
