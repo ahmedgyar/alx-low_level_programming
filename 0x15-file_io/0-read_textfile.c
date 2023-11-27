209bf66b908b64630d65a0fcfd569461cdf66c95
ahmedgyar <you@example.com>
2023-11-27 15:20:24 -0800

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,10 +4,8 @@
 /**
  * read_textfile - a function that reads a text file and prints it
  *                to POSIX standard output.
- *
  * @filename: is the file to read
  * @letters: number of letters to read and print from file
- *
  * Return: 0 if it fails or actual number of letters it could
  *         read and print
 */
