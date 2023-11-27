985df519485a4891837dc2d6adc5db537665c3c5
ahmedgyar <you@example.com>
2023-11-27 15:41:12 -0800

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,26 +9,25 @@
  * Return: 1 on success, -1 on failure
  */
 
-int create_file(const char *filename, char *text_content) {
+int create_file(const char *filename, char *text_content)
+{
 int fd;
 ssize_t write_res;
 
 if (filename == NULL)
-return -1;
-
+return (-1);
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
- return -1;
-
-if (text_content != NULL) {
+return (-1);
+if (text_content != NULL)
+{
 write_res = write(fd, text_content, strlen(text_content));
 if (write_res == -1)
 {
 close(fd);
-return -1;
+return (-1);
 }
 }
-
 close(fd);
-return 1;
+return (1);
 }
